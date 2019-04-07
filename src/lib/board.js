63c60d906ac152ea3e83059cd421831c205be651
Ewelina Łabas <ewelina.labas@gmail.com>
2019-04-07 17:24:30 +0200

    Fix alligned fields selection

--- src/lib/board.js
+++ src/lib/board.js
@@ -14,7 +14,8 @@ export const findAllEmptyNeighbours = (board, row, col) => {
   let neighbours = []
   for(let i = col - 1; i <= col + 1; i++) {
     for(let j = row - 1; j <= row + 1; j++){
-      neighbours.push(getField(board, j, i))
+      let field = getField(board, j, i)
+      if(field) {neighbours.push(field)}
     }
   }
   return neighbours.filter(f => !f.value).filter(f => !(f.row === row && f.col === col))
